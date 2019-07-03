f0018f5daeeb34ab90bf17794180d9dc8292748a
Ilya Vladimirskiy <ilya.vladimirskiy@smartfrog.com>
2019-07-03 15:38:35 +0200

    added first test, structured the code, using modules now

---
+++ tweefetcher/__init__.py
@@ -0,0 +1,1 @@
+import tweefetcher
--- tweefetcher/tweefetcher.py
+++ tweefetcher/tweefetcher.py
@@ -5,10 +5,10 @@ import argparse
 import os
 import unittest
 
-consumer_key = 't8bhToaY4LevkBjvS355A9pfu'
-consumer_secret = '2315cTZYr7OQmoDytBWpuothCAakOYER7pE6fF3ZLUXeAC8rkz'
-access_token_key = '882165583911038976-XgiJxECv0cCsg2Ech6HmzPuzsD1zILp'
-access_token_secret = 'e9puaP5NBK0JWjEUXCE214yc2CXpkgC1zPMzmgDs5ZLc5'
+CONSUMER_KEY = 't8bhToaY4LevkBjvS355A9pfu'
+CONSUMER_SECRET = '2315cTZYr7OQmoDytBWpuothCAakOYER7pE6fF3ZLUXeAC8rkz'
+ACCESS_TOKEN_KEY = '882165583911038976-XgiJxECv0cCsg2Ech6HmzPuzsD1zILp'
+ACCESS_TOKEN_SECRET = 'e9puaP5NBK0JWjEUXCE214yc2CXpkgC1zPMzmgDs5ZLc5'
 
 SCRIPT_NAME = os.path.basename(__file__)
 TWEETS_LIMIT = 5
@@ -16,23 +16,39 @@ VERSION = '1.0'
 BUILD_DATE = '03.07.2019'
 
 
+def init_tweeter_api(key=CONSUMER_KEY, secret=CONSUMER_SECRET,
+                     token_key=ACCESS_TOKEN_KEY, token_secret=ACCESS_TOKEN_SECRET):
+    """
+    Creates a connection to the Twitter API and returns it as an object
+    """
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
+
+
 def fetch_tweets(username, limit=TWEETS_LIMIT):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token_key, access_token_secret)
-
-    api = tweepy.API(auth)
-
-    tweets = api.user_timeline(screen_name=username)
-
-    test_tweet = tweets[0]
-    for key in test_tweet._json.keys():
-        print("{} : {}".format(key, test_tweet._json[key]))
-
-    tweets = [tweet.text for tweet in tweets]
-    print(list(tweets))
+    """
+    Fetches a set of tweets for the given user from Twitter API
+    """
+    api = init_tweeter_api()
+    tweets = api.user_timeline(screen_name=username, count=limit)
+    return tweets
 
 
-def main():
+def print_tweet(tweet, print_details=False):
+    if print_details:
+        for key in tweet._json.keys():
+            print(f'{key} : {tweet._json[key]}')
+    else:
+        print(f'{tweet.created_at}: {tweet.text}')
+
+
+def print_tweets(tweets, print_details=False):
+    for tweet in tweets:
+        print_tweet(tweet, print_details)
+
+
+def get_arguments():
     arg_parser = argparse.ArgumentParser(description='Fetches tweets for a specified user from the Twitter API, '
                                                      'stores them in local database and provides the possibility '
                                                      'to display them later along with some basic statistics')
@@ -42,10 +58,14 @@ def main():
     arg_parser.add_argument('-l', '--limit', action='store',
                             help=f'Limit for the number of tweets ({TWEETS_LIMIT} by default')
 
-    args = arg_parser.parse_args()
+    return arg_parser.parse_args()
+
+def main():
+    args = get_arguments()
 
     if args.action == 'fetch':
-        fetch_tweets(args.username, args.limit)
+        print(f'Fetching the last {args.limit} tweets for the user {args.username} from Twitter API')
+        print_tweets(fetch_tweets(args.username, args.limit), print_details=False)
     elif args.action == 'get':
         pass
     elif args.action == 'stats':

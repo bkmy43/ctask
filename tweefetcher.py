8853e376b54c0723fe6622ce0379a952a93e3546
Ilya Vladimirskiy <ilya.vladimirskiy@smartfrog.com>
2019-07-03 11:20:16 +0200

    Now the argumens are parsed and tweets for specific user are fetched from the API

--- tweefetcher.py
+++ tweefetcher.py
@@ -2,18 +2,20 @@
 
 import tweepy
 import argparse
+import os
 
 consumer_key = 't8bhToaY4LevkBjvS355A9pfu'
 consumer_secret = '2315cTZYr7OQmoDytBWpuothCAakOYER7pE6fF3ZLUXeAC8rkz'
 access_token_key = '882165583911038976-XgiJxECv0cCsg2Ech6HmzPuzsD1zILp'
 access_token_secret = 'e9puaP5NBK0JWjEUXCE214yc2CXpkgC1zPMzmgDs5ZLc5'
 
-TWEET_PER_PERSON_LIMIT_DEFAULT = 5
+SCRIPT_NAME = os.path.basename(__file__)
+TWEETS_LIMIT = 5
 VERSION = '1.0'
 BUILD_DATE = '03.07.2019'
 
 
-def get_tweets(username, limit=TWEET_PER_PERSON_LIMIT_DEFAULT):
+def fetch_tweets(username, limit=TWEETS_LIMIT):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token_key, access_token_secret)
 
@@ -33,17 +35,22 @@ def main():
     arg_parser = argparse.ArgumentParser(description='Fetches tweets for a specified user from the Twitter API, '
                                                      'stores them in local database and provides the possibility '
                                                      'to display them later along with some basic statistics')
-    arg_parser.add_argument('-v', '--version', action='version', version='%(prog)s v.{} {}'
-                            .format(VERSION, BUILD_DATE))
+    arg_parser.add_argument('-v', '--version', action='version', version=f'{SCRIPT_NAME} v.{VERSION} {BUILD_DATE}')
     arg_parser.add_argument('-a', '--action', action='store', help='Action to do: fetch/get/stats')
     arg_parser.add_argument('-u', '--username', action='store', help='Username to get the tweets for')
-    arg_parser.add_argument('-l', '--limit', action='store', help='Limit for the number of tweets ({} by default'
-                            .format(TWEET_PER_PERSON_LIMIT_DEFAULT))
+    arg_parser.add_argument('-l', '--limit', action='store',
+                            help=f'Limit for the number of tweets ({TWEETS_LIMIT} by default')
 
     args = arg_parser.parse_args()
-    print(args)
 
-    # get_tweets('navalny')
+    if args.action == 'fetch':
+        fetch_tweets(args.username, args.limit)
+    elif args.action == 'get':
+        pass
+    elif args.action == 'stats':
+        pass
+    else:
+        print(f'ERROR: No valid action specified, please run "{SCRIPT_NAME} -h" for help')
 
 
 if __name__ == "__main__":
